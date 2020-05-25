8f28d10db5302cbf6acc2ca51e28e2b1fd5476aa
Ahmad M <ahm4d3m@outlook.com>
2020-05-25 19:58:28 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/learsqlite/db/DatabaseContract.java
@@ -0,0 +1,19 @@
+package com.dicoding.learsqlite.db;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+    static String TABLE_NAME = "tes";
+
+    public static final class NoteColumns implements BaseColumns {
+        public static final String TABLE_NAME = "note";
+
+        //Note title
+        public static final String TITLE = "title";
+        //Note description
+        public static final String DESCRIPTION = "description";
+        //Note date
+        public static final String DATE = "date";
+
+    }
+}
